db38ee583f1671758bc0ad07521ba575ca556c02
IIIWinterIII <nikitavakulin39@gmail.com>
2023-12-01 15:03:37 +0300

    red App

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 function App() {
   return (
     <div class="calculator">
-    
+    <div class="display">
+      <input type="text" placeholder="0" />
+    </div>
 </div>
   );
 }
 
 export default App;
+
